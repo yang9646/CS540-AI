75241b9b4dbae83bd2e3dfb45e4c34e3b4a6a457
Hyukjoon Yang <hyang348@wisc.edu>
2021-10-07 15:55:14 -0500

    Main file

---
+++ HW3/pca.py
@@ -0,0 +1,27 @@
+from scipy.linalg import eigh
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def load_and_center_dataset(filename):
+    # TODO: add your code here
+
+
+def get_covariance(dataset):
+    # TODO: add your code here
+
+
+def get_eig(S, m):
+    # TODO: add your code here
+
+
+def get_eig_perc(S, perc):
+    # TODO: add your code here
+
+
+def project_image(img, U):
+    # TODO: add your code here
+
+
+def display_image(orig, proj):
+    # TODO: add your code here
